9187bb9589da7d1d36ea718c12aed7f83964fe04
walrusautotune <walrusautotune@protonmail.com>
2022-04-30 18:50:34 -0400

    adds creating variables

--- Program.cs
+++ Program.cs
@@ -2,11 +2,18 @@
 
 namespace VariablesCS
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            Epic();
+        }
+        static void Epic()
+        {
+            int numberOfCupOfCoffee = 2; 
+            string fullName = "Walrus Autotune";
+            DateTime today = DateTime.Now;
+            Console.WriteLine("Aloha, my name is {1}. I drink roughly {0} cups of coffee a day. Today is {2}, it's {2:h:mm tt} now.", numberOfCupOfCoffee, fullName, today);
+        }
     }
-  }
 }
